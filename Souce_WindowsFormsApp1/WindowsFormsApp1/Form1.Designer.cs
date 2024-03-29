35d011513aa204fd84b938c238c8a48bd6cb3385
fus <msnp@outlook.com.vn>
2024-03-30 00:07:04 +0700

    v1.2

---
+++ Souce_WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "8 bits or 10 bits from OSC:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DATA Extracted (MSB->LSB):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DATA Extracted (LSB->MSB):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(145, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Parity: ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(198, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "nothing ...";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(198, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "nothing ...";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(198, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "nothing ...";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(18, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(280, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "NOTE: 10 bits consist of 1 start bit, 8 data bits, 1 parity bit!";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label9.Location = new System.Drawing.Point(18, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(193, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "NOTE: 8 bits consist of only 8 data bits!";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label10.Location = new System.Drawing.Point(18, 75);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(279, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "NOTE: with 10bits input, start bit mus equal 1 (the first bit)!";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(271, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 13);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "nothing ...";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(271, 94);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 13);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "nothing ...";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 251);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Extract data-frame.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Souce_WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Reverse(string Input)
+        {
+
+            // Converting string to character array 
+            char[] charArray = Input.ToCharArray();
+
+            // Declaring an empty string
+            string reversedString = String.Empty;
+
+            // Iterating the each character from right to left 
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+
+                // Append each character to the reversedstring.
+                reversedString += charArray[i];
+            }
+
+            // Return the reversed string.
+            return reversedString;
+        }
+        public Tuple<char,char> get_DATA(String inp)
+        {
+            int res = 0;
+            int C_LSB_to_MSB = 0;
+            int C_MSB_to_LSB = 0;
+
+            if(inp.Length == 10)
+            {
+                C_MSB_to_LSB += (inp[1] == '1') ? (1 << 7) : (0);//MSB
+                C_MSB_to_LSB += (inp[2] == '1') ? (1 << 6) : (0);
+                C_MSB_to_LSB += (inp[3] == '1') ? (1 << 5) : (0);
+                C_MSB_to_LSB += (inp[4] == '1') ? (1 << 4) : (0);
+                C_MSB_to_LSB += (inp[5] == '1') ? (1 << 3) : (0);
+                C_MSB_to_LSB += (inp[6] == '1') ? (1 << 2) : (0);
+                C_MSB_to_LSB += (inp[7] == '1') ? (1 << 1) : (0);
+                C_MSB_to_LSB += (inp[8] == '1') ? (1 << 0) : (0);//LSB
+
+                C_LSB_to_MSB += (inp[1] == '1') ? (1 << 0) : (0);//MSB
+                C_LSB_to_MSB += (inp[2] == '1') ? (1 << 1) : (0);
+                C_LSB_to_MSB += (inp[3] == '1') ? (1 << 2) : (0);
+                C_LSB_to_MSB += (inp[4] == '1') ? (1 << 3) : (0);
+                C_LSB_to_MSB += (inp[5] == '1') ? (1 << 4) : (0);
+                C_LSB_to_MSB += (inp[6] == '1') ? (1 << 5) : (0);
+                C_LSB_to_MSB += (inp[7] == '1') ? (1 << 6) : (0);
+                C_LSB_to_MSB += (inp[8] == '1') ? (1 << 7) : (0);//LSB
+            }
+            else if(inp.Length == 8)
+            {
+                C_MSB_to_LSB += (inp[0] == '1') ? (1 << 7) : (0);//MSB
+                C_MSB_to_LSB += (inp[1] == '1') ? (1 << 6) : (0);
+                C_MSB_to_LSB += (inp[2] == '1') ? (1 << 5) : (0);
+                C_MSB_to_LSB += (inp[3] == '1') ? (1 << 4) : (0);
+                C_MSB_to_LSB += (inp[4] == '1') ? (1 << 3) : (0);
+                C_MSB_to_LSB += (inp[5] == '1') ? (1 << 2) : (0);
+                C_MSB_to_LSB += (inp[6] == '1') ? (1 << 1) : (0);
+                C_MSB_to_LSB += (inp[7] == '1') ? (1 << 0) : (0);//LSB
+
+                C_LSB_to_MSB += (inp[0] == '1') ? (1 << 0) : (0);//MSB
+                C_LSB_to_MSB += (inp[1] == '1') ? (1 << 1) : (0);
+                C_LSB_to_MSB += (inp[2] == '1') ? (1 << 2) : (0);
+                C_LSB_to_MSB += (inp[3] == '1') ? (1 << 3) : (0);
+                C_LSB_to_MSB += (inp[4] == '1') ? (1 << 4) : (0);
+                C_LSB_to_MSB += (inp[5] == '1') ? (1 << 5) : (0);
+                C_LSB_to_MSB += (inp[6] == '1') ? (1 << 6) : (0);
+                C_LSB_to_MSB += (inp[7] == '1') ? (1 << 7) : (0);//LSB
+            }
+            return new Tuple<char,char>( (char) C_LSB_to_MSB, (char)C_MSB_to_LSB);
+        }
+        public char get_Parity(String inp)
+        {
+            int p = 0;
+            for (int i = 0; i < inp.Length; i++)
+                p += (bool)(inp[i] == '1')?(1):(0);
+            return (char)('0' + p % 2);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inp = textBox1.Text;
+            Tuple<char, char> DATA = get_DATA(inp);
+            char P = get_Parity(inp);
+            label5.Text = DATA.Item2.ToString();
+            label6.Text = DATA.Item1.ToString();
+            if(inp.Length == 10) label7.Text = P.ToString();
+            
+            if ('A' < DATA.Item2 && DATA.Item2 < 'Z' || 'a' < DATA.Item2 && DATA.Item2 < 'z')
+                label12.Text = (inp.Length==10)?inp.Substring(1, 8):inp;
+            else
+                label12.ResetText();
+            
+            if ('A' < DATA.Item1 && DATA.Item1 < 'Z' || 'a' < DATA.Item1 && DATA.Item1 < 'z')
+                label11.Text = Reverse((inp.Length == 10) ? inp.Substring(1, 8) : inp);
+            else
+                label11.ResetText();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string inp = textBox1.Text;
+            if(inp.Length == 10 || inp.Length == 8)
+            {
+                Tuple<char, char> DATA = get_DATA(inp);
+                char P = get_Parity(inp);
+                label5.Text = DATA.Item2.ToString();
+                label6.Text = DATA.Item1.ToString();
+                if (inp.Length == 10) label7.Text = P.ToString();
+                if ('A' < DATA.Item2 && DATA.Item2 < 'Z' || 'a' < DATA.Item2 && DATA.Item2 < 'z')
+                    label12.Text = (inp.Length == 10) ? inp.Substring(1, 8) : inp;
+                else
+                    label11.ResetText();
+                if ('A' < DATA.Item1 && DATA.Item1 < 'Z' || 'a' < DATA.Item1 && DATA.Item1 < 'z')
+                    label11.Text = Reverse((inp.Length == 10) ? inp.Substring(1, 8) : inp);
+                else
+                    label11.ResetText();
+            }
+            else
+            {
+                label5.Text = "nothing ...";
+                label6.Text = "nothing ...";
+                label7.Text = "nothing ...";
+                label12.ResetText();
+                label11.ResetText();
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
